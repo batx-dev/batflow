1282cc94f65ad8db51f2d85fe5b0f69270af1e9d
Taizeng Wu <wutaizeng@gmail.com>
2023-04-04 15:28:11 +0800

    feat: add slurm apptainer activity

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/batx-dev/batflow
 
 go 1.20
 
-require go.temporal.io/sdk v1.21.1
+require (
+	go.temporal.io/sdk v1.21.1
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	k8s.io/apimachinery v0.26.3
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -21,9 +25,9 @@ require (
 	github.com/stretchr/testify v1.8.1 // indirect
 	go.temporal.io/api v1.16.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	golang.org/x/time v0.1.0 // indirect
 	google.golang.org/genproto v0.0.0-20230127162408-596548ed4efa // indirect
 	google.golang.org/grpc v1.52.3 // indirect
---
+++ slurm_apptainer.go
@@ -0,0 +1,121 @@
+package batflow
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"path"
+	"strings"
+
+	"golang.org/x/crypto/ssh"
+	"k8s.io/apimachinery/pkg/util/rand"
+)
+
+const (
+	DeviceNvidiaGPUKey = "nvidia.com/gpu"
+)
+
+type Container struct {
+	// Name of the container specified as a DNS_LABEL. Each Container must
+	// have a unique name.
+	Name string `json:"name"`
+	// Image container image name.
+	Image Image `json:"image"`
+	// Command to execute.
+	Command []string `json:"command"`
+	// Ports list of port to expose from the container.
+	Ports []uint16 `json:"ports"`
+	// Resources compute resources required by container.
+	Resource Resource `json:"resource"`
+}
+
+// Image container image name.
+type Image struct {
+	URI url.URL `json:"uri"`
+}
+
+// Resource compute resources required by container.
+type Resource struct {
+	CPU struct {
+		Cores uint `json:"cores"`
+	}
+	Memory struct {
+		Size string `json:"size"`
+	}
+	Devices map[string]uint `json:"devices"`
+}
+
+type SlurmApptainerActivities struct {
+	sshClient *ssh.Client
+}
+
+// Start a named instance of the given container image
+func (a *SlurmApptainerActivities) Start(ctx context.Context, container *Container) (err error) {
+	sess, err := a.sshClient.NewSession()
+	if err != nil {
+		return fmt.Errorf("ssh new session: %v", err)
+	}
+	defer sess.Close()
+
+	if err := sess.Run(buildSlurmJobSubmitCommand(container)); err != nil {
+		return fmt.Errorf("submit job: %v", err)
+	}
+
+	return nil
+}
+
+// List all running and named container instances
+func (a *SlurmApptainerActivities) List(ctx context.Context, names []string) (err error) {
+	return
+}
+
+func buildSlurmJobSubmitCommand(container *Container) string {
+	if container.Name == "" {
+		container.Name = rand.String(16)
+	}
+	workDir := path.Join("~/.batflow/jobs", container.Name)
+
+	return `sh <<- 'CMD'
+		mkdir -p ` + workDir + ` || exit $?
+		cd ` + workDir + ` || exit $?
+
+		# Generate job script.
+		cat <<- 'SCRIPT' > job.sh || exit $?
+			#/usr/bin/env bash
+			` + buildSlurmJobOptions(container) + `
+
+			[[ -f ~/.batflow/env ]] && source ~/.batflow/env || true
+			` + buildSlurmJobStepCommand(container) + `
+		SCRIPT
+
+		# Submit job script.
+		sbatch job.sh
+	CMD`
+}
+
+func buildSlurmJobOptions(container *Container) string {
+	opts := []string{
+		fmt.Sprintf("--job-name=%q", container.Name),
+	}
+	if count, ok := container.Resource.Devices[DeviceNvidiaGPUKey]; ok {
+		opts = append(opts, fmt.Sprintf("--gpus=%d", count))
+	}
+	for i, opt := range opts {
+		opts[i] = "#SBATCH " + opt
+	}
+	return strings.Join(opts, "\n")
+}
+
+func buildSlurmJobStepCommand(container *Container) string {
+	command := []string{
+		"apptainer",
+		"exec",
+		"--writable-tmpfs",
+	}
+	if _, ok := container.Resource.Devices[DeviceNvidiaGPUKey]; ok {
+		command = append(command, "--nv")
+	}
+	command = append(command, container.Image.URI.String())
+	command = append(command, container.Command...)
+	return strings.Join(command, " ")
+}
